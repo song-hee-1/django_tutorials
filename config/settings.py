db10564ce2669cd8d1767af1ed97dded441409a7
한송희 <98_han@naver.com>
2022-07-19 20:46:43 +0900

    Feat : database setup
    - run migrate to database set up
    - create models (question, choice)
    - activating models
    - make the poll app modifiable in the admin

--- config/settings.py
+++ config/settings.py
@@ -47,6 +47,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
